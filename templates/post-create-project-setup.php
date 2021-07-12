3d44f472ca551a7dfb3a89c457ec39e094bd75ef
mdupree <mdupree@acromedia.com>
2021-07-12 13:14:28 -0700

    more configuration

--- templates/post-create-project-setup.php
+++ templates/post-create-project-setup.php
@@ -7,9 +7,6 @@ $default_webroot = 'web';
 
 echo "Setting up your drupal project!\n";
 
-/*
- * Collect some information
- */
 echo "What is the name of your project [$default_project_name] :";
 $default_project_name = preg_replace("/\s+|_+/", '-', rtrim(fgets(STDIN))) ?: $default_project_name;
 echo "project name: " . $default_project_name . "\n";
@@ -18,34 +15,58 @@ echo "What is webroot of your project [$default_webroot] :";
 $default_webroot = preg_replace("/\s+|_+/", '-', rtrim(fgets(STDIN))) ?: $default_webroot;
 echo "project webroot: " . $default_webroot . "\n";
 
-/*
- * Copy template files
- */
 echo "Copying template files...\n";
 copy($templates_path . 'template.README.md', $project_path . '/README.md');
 copy($templates_path . 'template.gitlab-ci.yml', $project_path . '/.gitlab-ci.yml');
-copy($templates_path . 'templates/template.composer.json', $project_path . '/composer.json');
+copy($templates_path . 'template.gitignore', $project_path . '/.gitignore');
+copy($templates_path . 'template.composer.json', $project_path . '/composer.json');
+copy($templates_path . 'template.grumphp.yml.dist', $project_path . '/grumphp.yml.dist');
 copy($templates_path . 'template.lando.yml', $project_path . '/lando.yml');
 copy($templates_path . 'lagoonize/template.env', $project_path . '/.env');
 copy($templates_path . 'lagoonize/template.lagoon.yml', $project_path . '/.lagoon.yml');
-copy($templates_path . 'lagoonize/lagoon/cli.dockerfile', $project_path . '/lagoon/cli.dockerfile');
 
-/*
- * Replace tokens in template files
- */
-replace_string_in_file($project_path . '/lando.yml', '[PROJECTNAME]', $default_project_name);
-replace_string_in_file($project_path . '/lando.yml', '[WEBROOT]', $default_webroot);
-replace_string_in_file($project_path . '/lagoon/cli.dockerfile', '[WEBROOT]', $default_webroot);
-replace_string_in_file($project_path . '/lagoon/nginx.dockerfile', '[WEBROOT]', $default_webroot);
-replace_string_in_file($project_path . '/.env', '[PROJECTNAME]', $default_project_name);
+$token_replacments = [
+    '[PROJECTNAME]' => $default_project_name,
+    '[WEBROOT]'     => $default_webroot
+];
 
+replace_file_token($project_path . '/lando.yml', $token_replacments);
+replace_file_token($project_path . '/lagoon-images/cli.dockerfile', $token_replacments);
+replace_file_token($project_path . '/lagoon-images/nginx.dockerfile', $token_replacments);
+replace_file_token($project_path . '/.env', $token_replacments);
 
+delete_files($templates_path);
 echo "Finishing the project setup!\nRemember to update the .gitlab-ci.yml variables for deploy jobs\n";
 
-
-function replace_string_in_file($filename, $string_to_replace, $replace_with){
+/**
+ * Replace file tokens with value.
+ *
+ * @param $filename
+ * @param $replacement
+ *   ['TOKEN' => 'Value']
+ */
+function replace_file_token($filename, $replacement) {
     $content = file_get_contents($filename);
-    $content_chunks = explode($string_to_replace, $content);
-    $content = implode($replace_with, $content_chunks);
+    foreach($replacement as $token => $value) {
+        $content_chunks = explode($token, $content);
+        $content = implode($value, $content_chunks);
+    }
     file_put_contents($filename, $content);
+}
+
+/*
+ * php delete function that deals with directories recursively
+ */
+function delete_files($target) {
+    if(is_dir($target)){
+        $files = glob( $target . '*', GLOB_MARK ); //GLOB_MARK adds a slash to directories returned
+
+        foreach( $files as $file ){
+            delete_files( $file );
+        }
+
+        rmdir( $target );
+    } elseif(is_file($target)) {
+        unlink( $target );
+    }
 }
